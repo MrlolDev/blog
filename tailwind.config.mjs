6c8d3653ffd4dd18dd10e95bf9fc64ca071c3828
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2023-11-06 05:48:20 +0000

    Update styles and remove unused files

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				theme: {
+					background: "#09090b",
+					foreground:"#fafafa",
+					border: "#27272a",
+					ring: "#d4d4d8",
+					destructive: "#7f1d1d",
+				},
+				brand: {
+					primary:"#fafbfb",
+					secondary: "#27262b",
+				}
+			}
+		},
 	},
 	plugins: [],
 }
