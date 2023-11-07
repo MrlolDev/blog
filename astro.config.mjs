5edacf9df9f0ecadd563102abd1eb1504eacb6c3
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2023-11-07 15:22:35 +0000

    Remove GitHub Pages deployment workflow and update Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mrloldev.github.io',
-  base: '/blog',
   integrations: [mdx(), sitemap(), tailwind()]
 });
