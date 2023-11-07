85c64bd154d30c9c5768777d3a3e3a6519ca6537
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2023-11-07 12:38:45 +0000

    Update Astro config and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mrloldev.github.io',
-  base: '/blog/',
+  base: '/blog',
   integrations: [mdx(), sitemap(), tailwind()]
 });
