5dd966f7c0c47a4a7616d0c1acf4470928e30e28
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2023-11-07 12:06:17 +0000

    Deploy on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://mrloldev.github.io',
+  base: '/blog/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mrlol blog';
+export const SITE_TITLE = 'Mrlol.dev';
 export const SITE_DESCRIPTION = 'Bienvenid@ a mi blog, donde intentaré explicar conceptos de la tecnología y de la AI muy complejos de una manera hiper simple.';
