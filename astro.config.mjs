f353beae8f7d5267877c907a94dba942de711edf
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2023-11-17 10:00:59 +0000

    feat: multi lang

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mrlol-me',
+  site: "https://mrlol.me",
   experimental: {
-    
+    i18n: {
+      defaultLocale: "en",
+      locales: ["en", "es"],
+      routingStrategy: "prefix-always",
+    },
   },
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
 });
