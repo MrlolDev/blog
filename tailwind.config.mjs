c4ef332c19b0180637ec9d19c81a4be985ba7f7f
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-11-29 06:54:03 +0100

    feat: transitions + multitheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["class"],
+
   theme: {
     extend: {
       colors: {
-        theme: {
+        dark: {
           background: "#09090b",
           foreground: "#fafafa",
           border: "#27272a",
           ring: "#d4d4d8",
           destructive: "#7f1d1d",
+          text: "#fafafa",
+        },
+        theme: {
+          background: "#f0f0f0",
+          foreground: "#333333",
+          border: "#cccccc",
+          ring: "#ffffff",
+          destructive: "#ff6666",
+          text: "#333333",
         },
         brand: {
-          primary: "#fafbfb",
-          secondary: "#27262b",
-        },
-        lol: {
           primary: "#ffe000",
           secondary: "#019cdb",
         },
